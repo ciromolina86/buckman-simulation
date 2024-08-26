9383f4f99dafdaf3ffc7a48d2f3d069f1b6f6263
Ciro Molina <cmolina@itgtec.com>
2024-08-25 22:02:09 -0400

    class Analog_Filt_Scale is complete (read_values, write_values). method write_area was implemented in class S7_Manager. test is complete and working

--- handlers/comm_handler.py
+++ handlers/comm_handler.py
@@ -41,104 +41,47 @@ class S7_Manager:
     def get_cpu_info(self):
         result = self.parse_S7CpuInfo(self._plc.get_cpu_info())
         result.update(self.parse_S7OrderCode(self._plc.get_order_code()))
-        return result
+        return json.dumps(result, indent=4)
 
     # get datetime from PLC
     def get_plc_datetime(self):
         pass
 
-    def read_area(self, area: Area, db_number: int, start_byte: int, number_of_bytes: int):
-        return self._plc.read_area(area, db_number, start_byte, number_of_bytes)
+    def read_area(self, area: Area, db_number: int, start_byte: int, number_of_bytes: int) -> bytearray:
+        """
+        Read a data area from a PLC: DB, Inputs, Outputs, Memory, Timers and Counters.
 
-    # result = snap7.util.get_real(result, 0)
-    # def get_tag_list(self):
-    #     pass
-    # with self._plc as plc:
-    #     return plc.get_tag_list()
+        Args:
+            area: area to be read from.
+            db_number: The DB number, only used when area=Areas.DB
+            start_byte: byte index to start reading.
+            number_of_bytes: number of bytes to read.
 
-    # def get_tag_list_json(self):
-    #     pass
-    #     # with self._plc as plc:
-    #     #     return plc.tags_json
+        Returns:
+            Buffer with the data read.
+        """
+        try:
+            return self._plc.read_area(area, db_number, start_byte, number_of_bytes)
+        except Exception as e:
+            print(e)
 
-    # def read_single_tag(self, tag_name: str = 'testDINT'):
-    #     pass
-    #     # # Reading a single tag returns a Tag object
-    #     # # e.g. Tag(tag='testDINT', value=20, type='DINT', error=None)
-    #     #
-    #     # with self._plc as plc:
-    #     #     result = plc.read(tag_name)
-    #     #     # print(result)
-    #     #     if result.error is None:
-    #     #         # print(f'{datetime.datetime.now()}\t|\tReading successfully from \t{tag_name}: {result.value:.2f}')
-    #     #         return result.value
-    #     #     else:
-    #     #         print(f'{datetime.datetime.now()}\t|\tError reading from \t{tag_name}: {result.error}')
-    #     #         return None
+    def write_area(self, area: Area, db_number: int, start_byte: int, data: bytearray) -> int:
+        """
+        Writes a data area into a PLC.
 
-    # def read_multiple_tags(self, tag_names: list = ['testDINT', 'testSine']):
-    #     pass
-    #     # # Reading multiple tags returns a list of Tag objects
-    #     # # e.g. [Tag(tag='testDINT', value=20, type='DINT', error=None),
-    #     # #       Tag(tag='testSine', value=100.36, type='REAL', error=None)]
-    #     #
-    #     # values = []
-    #     # with self._plc as plc:
-    #     #     results = plc.read(*tag_names)
-    #     #
-    #     #     for tag_name, result in zip(tag_names, results):
-    #     #         if result.error is None:
-    #     #             values.append(result.value)
-    #     #         else:
-    #     #             print(f'Error reading {tag_name}: {result.error}')
-    #     #             values.append(None)
-    #     #
-    #     # return values
+        Args:
+            area: area to be written.
+            db_number: number of the db to be written to. In case of Inputs, Marks or Outputs, this should be equal to 0
+            start: byte index to start writting.
+            data: buffer to be written.
 
-    # def read_udt(self, inst_name: str = 'Tank1'):
-    #     pass
-    #     # # Structures can be read as a whole, assuming that no attributes have External Access set to None.
-    #     # # Structure tags will be a single Tag object, but the value attribute will be a dict of {attribute: value}.
-    #     # # e.g. Tag(tag='Tank1', value={'Level': 100.5, 'Level_EU': "ft", 'Volume': 1000, 'Volume_EU': "gal"},
-    #     # #          type='udtTank', error=None)
-    #     #
-    #     # with self._plc as plc:
-    #     #     return plc.read(inst_name)
-
-    # def write_single_tag(self, tag_name: str = 'testSine', tag_value=None):
-    #     pass
-    #     # # Writing a single tag returns a single Tag object response
-    #     # # e.g. Tag(tag='testSine', value=100.5, type='REAL', error=None)
-    #     #
-    #     # with self._plc as plc:
-    #     #     result = plc.write((tag_name, tag_value))
-    #     #     # print(result)
-    #     #
-    #     # if result.error is None:
-    #     #     # print(f'{datetime.datetime.now()}\t|\tWriting successfully to \t{tag_name}: {result.value:.2f}')
-    #     #     return result.value
-    #     # else:
-    #     #     print(f'{datetime.datetime.now()}\t|\tError writing to \t{tag_name}: {result.error}')
-    #     #     return None
-
-    # def write_multiple_tags(self, tag_names: list = ['testSine', 'testDINT'], tag_values: list = [None, None]):
-    #     pass
-    #     # # Writing multiple tags will return a list of Tag objects
-    #     # # e.g. [Tag(tag='testSine', value=25.2, type='REAL', error=None),
-    #     # #       Tag(tag='testDINT', value=20, type='DINT', error=None)]
-    #     #
-    #     # with self._plc as plc:
-    #     #     data = list(zip(tag_names, tag_values))
-    #     #     results = plc.write(*data)
-    #     #     # print(type(result),result)
-    #     #
-    #     # for tag_name, result in zip(tag_names, results):
-    #     #     if result.error is None:
-    #     #         # print(f'{datetime.datetime.now()}\t|\tWriting successfully to \t{tag_name}: {result.value:.2f}')
-    #     #         return result.value
-    #     #     else:
-    #     #         print(f'{datetime.datetime.now()}\t|\tError writing to \t{tag_name}: {result.error}')
-    #     #         return None
+        Returns:
+            Snap7 error code.
+        """
+        try:
+            self._plc.write_area(area, db_number, start_byte, data)
+        except Exception as e:
+            print(e)
 
 
 class Analog_Filt_Scale:
@@ -170,14 +113,14 @@ class Analog_Filt_Scale:
         self.Cmd_Proc_En = get_bool(self._buffer, 52, 4)
         self.Cmd_Warn_En = get_bool(self._buffer, 52, 5)
         self.Cmd_Alarm_En = get_bool(self._buffer, 52, 6)
-        self.Cmd_Sim_Value = get_real(self._buffer, 54)
-        self.Cfg_High_Range = get_real(self._buffer, 58)
-        self.Cfg_Low_Range = get_real(self._buffer, 62)
-        self.Cfg_Scale_Offset = get_real(self._buffer, 66)
-        self.Cfg_High_Proc = get_real(self._buffer, 70)
-        self.Cfg_Low_Proc = get_real(self._buffer, 74)
-        self.Cfg_High_Warn = get_real(self._buffer, 78)
-        self.Cfg_Low_Warn = get_real(self._buffer, 82)
+        self.Cmd_Sim_Value = round(get_real(self._buffer, 54), 2)
+        self.Cfg_High_Range = round(get_real(self._buffer, 58), 2)
+        self.Cfg_Low_Range = round(get_real(self._buffer, 62), 2)
+        self.Cfg_Scale_Offset = round(get_real(self._buffer, 66), 2)
+        self.Cfg_High_Proc = round(get_real(self._buffer, 70), 2)
+        self.Cfg_Low_Proc = round(get_real(self._buffer, 74), 2)
+        self.Cfg_High_Warn = round(get_real(self._buffer, 78), 2)
+        self.Cfg_Low_Warn = round(get_real(self._buffer, 82), 2)
         self.Cfg_Range_Timer = get_time(self._buffer, 86)
         self.Cfg_Proc_Timer = get_time(self._buffer, 90)
         self.Cfg_Warn_Timer = get_time(self._buffer, 94)
@@ -186,7 +129,7 @@ class Analog_Filt_Scale:
         self.Stat_Auto = get_bool(self._buffer, 104, 0)
         self.Stat_Sim = get_bool(self._buffer, 104, 1)
         self.Stat_Raw = get_int(self._buffer, 106)
-        self.Stat_Scale = get_real(self._buffer, 108)
+        self.Stat_Scale = round(get_real(self._buffer, 108), 2)
         self.Stat_Fault = get_bool(self._buffer, 112, 0)
         self.Stat_High_Proc = get_bool(self._buffer, 112, 1)
         self.Stat_Low_Proc = get_bool(self._buffer, 112, 2)
@@ -195,67 +138,109 @@ class Analog_Filt_Scale:
         self.Cfg_Return_Auto = get_time(self._buffer, 114)
 
     def __str__(self):
-        return json.dumps(self.get_all_values(),indent=4)
+        return json.dumps(self.read_values(), indent=4)
 
+    def get_attributes(self):
+        return list(self.read_values().keys())
 
-    def get_all_values(self):
+    def read_values(self):
         result = {}
         result.update({'Raw_Input': self.Raw_Input})
         result.update({'Def_High_Range': self.Def_High_Range})
-        result.update({'Def_Low_Range':self.Def_Low_Range})
-        result.update({'Def_Low_Proc':self.Def_Low_Proc})
-        result.update({'Def_High_Warn':self.Def_High_Warn})
-        result.update({'Def_Low_Warn':self.Def_Low_Warn})
-        result.update({'Def_Sample_Time':self.Def_Sample_Time})
-        result.update({'Def_Range_Timer':self.Def_Range_Timer})
-        result.update({'Def_Proc_Timer':self.Def_Proc_Timer})
-        result.update({'Def_Warn_Timer':self.Def_Warn_Timer})
-        result.update({'Cmd_Reset_Alarm':self.Cmd_Reset_Alarm})
-        result.update({'Stat_PV':self.Stat_PV})
-        result.update({'Stat_High_Range':self.Stat_High_Range})
-        result.update({'Stat_Low_Range':self.Stat_Low_Range})
-        #
-        #
-        # f"'Cmd_Clear_Table':{self.Cmd_Clear_Table}, 'Cmd_Filter_En':{self.Cmd_Filter_En}, " \
-        # f"'Cmd_Auto':{self.Cmd_Auto},'Cmd_Sim':{self.Cmd_Sim}, 'Cmd_Proc_En':{self.Cmd_Proc_En}, " \
-        # f"'Cmd_Warn_En':{self.Cmd_Warn_En}, 'Cmd_Alarm_En':{self.Cmd_Alarm_En}, " \
-        # f"'Cmd_Sim_Value':{self.Cmd_Sim_Value}, 'Cfg_High_Range':{self.Cfg_High_Range}," \
-        # f"'Cfg_Low_Range':{self.Cfg_Low_Range}, 'Cfg_Scale_Offset':{self.Cfg_Scale_Offset}," \
-        # f"'Cfg_High_Proc:{self.Cfg_High_Proc}', 'Cfg_Low_Proc:{self.Cfg_Low_Proc}', " \
-        # f"'Cfg_High_Warn':{self.Cfg_High_Warn},     'Cfg_Low_Warn':{self.Cfg_Low_Warn}, " \
-        # f"'Cfg_Range_Timer':{self.Cfg_Range_Timer},     'Cfg_Proc_Timer':{self.Cfg_Proc_Timer}, " \
-        # f"'Cfg_Warn_Timer':{self.Cfg_Warn_Timer},     'Cfg_Num_Of_Samp':{self.Cfg_Num_Of_Samp}, " \
-        # f"'Cfg_Sample_time':{self.Cfg_Sample_time}, 'Stat_Auto':{self.Stat_Auto}," \
-        # f"'Stat_Sim':{self.Stat_Sim}, ''Stat_Raw':{self.Stat_Raw}, " \
-        # f"'Stat_Scale':{self.Stat_Scale},     'Stat_Fault':{self.Stat_Fault}, " \
-        # f"'Stat_High_Proc':{self.Stat_High_Proc}, 'Stat_Low_Proc':{self.Stat_Low_Proc}," \
-        # f"'Stat_High_Warn':{self.Stat_High_Warn}, 'Stat_Low_Warn':{self.Stat_Low_Warn}," \
-        # f"'Cfg_Return_Auto':{self.Cfg_Return_Auto}}}"
-        # result.update({})
-        # result.update({})
-        # result.update({})
-        # result.update({})
-        # result.update({})
-        # result.update({})
-        # result.update({})
-        # result.update({})
-        # result.update({})
-        # result.update({})
-        # result.update({})
-        # result.update({})
-        # result.update({})
-        # result.update({})
-        # result.update({})
-        # result.update({})
-        # result.update({})
-        # result.update({})
-        # result.update({})
-        # result.update({})
-        # result.update({})
-        # result.update({})
-        # result.update({})
+        result.update({'Def_Low_Range': self.Def_Low_Range})
+        result.update({'Def_Low_Proc': self.Def_Low_Proc})
+        result.update({'Def_High_Warn': self.Def_High_Warn})
+        result.update({'Def_Low_Warn': self.Def_Low_Warn})
+        result.update({'Def_Sample_Time': self.Def_Sample_Time})
+        result.update({'Def_Range_Timer': self.Def_Range_Timer})
+        result.update({'Def_Proc_Timer': self.Def_Proc_Timer})
+        result.update({'Def_Warn_Timer': self.Def_Warn_Timer})
+        result.update({'Cmd_Reset_Alarm': self.Cmd_Reset_Alarm})
+        result.update({'Stat_PV': self.Stat_PV})
+        result.update({'Stat_High_Range': self.Stat_High_Range})
+        result.update({'Stat_Low_Range': self.Stat_Low_Range})
+        result.update({'Cmd_Clear_Table': self.Cmd_Clear_Table})
+        result.update({'Cmd_Filter_En': self.Cmd_Filter_En})
+        result.update({'Cmd_Auto': self.Cmd_Auto})
+        result.update({'Cmd_Sim': self.Cmd_Sim})
+        result.update({'Cmd_Proc_En': self.Cmd_Proc_En})
+        result.update({'Cmd_Warn_En': self.Cmd_Warn_En})
+        result.update({'Cmd_Alarm_En': self.Cmd_Alarm_En})
+        result.update({'Cmd_Sim_Value': self.Cmd_Sim_Value})
+        result.update({'Cfg_High_Range': self.Cfg_High_Range})
+        result.update({'Cfg_Low_Range': self.Cfg_Low_Range})
+        result.update({'Cfg_Scale_Offset': self.Cfg_Scale_Offset})
+        result.update({'Cfg_High_Proc': self.Cfg_High_Proc})
+        result.update({'Cfg_Low_Proc': self.Cfg_Low_Proc})
+        result.update({'Cfg_High_Warn': self.Cfg_High_Warn})
+        result.update({'Cfg_Low_Warn': self.Cfg_Low_Warn})
+        result.update({'Cfg_Range_Timer': self.Cfg_Range_Timer})
+        result.update({'Cfg_Proc_Timer': self.Cfg_Proc_Timer})
+        result.update({'Cfg_Warn_Timer': self.Cfg_Warn_Timer})
+        result.update({'Cfg_Num_Of_Samp': self.Cfg_Num_Of_Samp})
+        result.update({'Cfg_Sample_time': self.Cfg_Sample_time})
+        result.update({'Stat_Auto': self.Stat_Auto})
+        result.update({'Stat_Sim': self.Stat_Sim})
+        result.update({'Stat_Raw': self.Stat_Raw})
+        result.update({'Stat_Scale': self.Stat_Scale})
+        result.update({'Stat_Fault': self.Stat_Fault})
+        result.update({'Stat_High_Proc': self.Stat_High_Proc})
+        result.update({'Stat_Low_Proc': self.Stat_Low_Proc})
+        result.update({'Stat_High_Warn': self.Stat_High_Warn})
+        result.update({'Stat_Low_Warn': self.Stat_Low_Warn})
+        result.update({'Cfg_Return_Auto': self.Cfg_Return_Auto})
 
         return result
 
-    def get_all_attributes(self):
-        return list(self.get_all_values().keys())
+    def write_values(self):
+        # Inputs
+        set_int(self._buffer, 0, self.Raw_Input)
+        set_real(self._buffer, 2, self.Def_High_Range)
+        set_real(self._buffer, 6, self.Def_Low_Range)
+        set_real(self._buffer, 10, self.Def_High_Proc)
+        set_real(self._buffer, 14, self.Def_Low_Proc)
+        set_real(self._buffer, 18, self.Def_High_Warn)
+        set_real(self._buffer, 22, self.Def_Low_Warn)
+        set_int(self._buffer, 26, self.Def_Num_Samp)
+        set_time(self._buffer, 28, self.Def_Sample_Time)
+        set_time(self._buffer, 32, self.Def_Range_Timer)
+        set_time(self._buffer, 36, self.Def_Proc_Timer)
+        set_time(self._buffer, 40, self.Def_Warn_Timer)
+        set_bool(self._buffer, 44, 0, self.Cmd_Reset_Alarm)
+        # Outputs
+        # set_real(self._buffer, 46, self.Stat_PV)
+        # set_bool(self._buffer, 50, 0, self.Stat_High_Range)
+        # set_bool(self._buffer, 50, 1, self.Stat_Low_Range)
+        # Static
+        set_bool(self._buffer, 52, 0, self.Cmd_Clear_Table)
+        set_bool(self._buffer, 52, 1, self.Cmd_Filter_En)
+        set_bool(self._buffer, 52, 2, self.Cmd_Auto)
+        set_bool(self._buffer, 52, 3, self.Cmd_Sim)
+        set_bool(self._buffer, 52, 4, self.Cmd_Proc_En)
+        set_bool(self._buffer, 52, 5, self.Cmd_Warn_En)
+        set_bool(self._buffer, 52, 6, self.Cmd_Alarm_En)
+        set_real(self._buffer, 54, self.Cmd_Sim_Value)
+        set_real(self._buffer, 58, self.Cfg_High_Range)
+        set_real(self._buffer, 62, self.Cfg_Low_Range)
+        set_real(self._buffer, 66, self.Cfg_Scale_Offset)
+        set_real(self._buffer, 70, self.Cfg_High_Proc)
+        set_real(self._buffer, 74, self.Cfg_Low_Proc)
+        set_real(self._buffer, 78, self.Cfg_High_Warn)
+        set_real(self._buffer, 82, self.Cfg_Low_Warn)
+        set_time(self._buffer, 86, self.Cfg_Range_Timer)
+        set_time(self._buffer, 90, self.Cfg_Proc_Timer)
+        set_time(self._buffer, 94, self.Cfg_Warn_Timer)
+        set_int(self._buffer, 98, self.Cfg_Num_Of_Samp)
+        set_time(self._buffer, 100, self.Cfg_Sample_time)
+        # set_bool(self._buffer, 104, 0, self.Stat_Auto)
+        # set_bool(self._buffer, 104, 1, self.Stat_Sim)
+        # set_int(self._buffer, 106, self.Stat_Raw)
+        # set_real(self._buffer, 108, self.Stat_Scale)
+        # set_bool(self._buffer, 112, 0, self.Stat_Fault)
+        # set_bool(self._buffer, 112, 1, self.Stat_High_Proc)
+        # set_bool(self._buffer, 112, 2, self.Stat_Low_Proc)
+        # set_bool(self._buffer, 112, 3, self.Stat_High_Warn)
+        # set_bool(self._buffer, 112, 4, self.Stat_Low_Warn)
+        set_time(self._buffer, 114, self.Cfg_Return_Auto)
+
+        return self._buffer
--- testing.py
+++ testing.py
@@ -1,63 +1,30 @@
 from handlers.comm_handler import *
 import snap7
-from snap7.type import Area, BlocksList, Block
-
-def test1():
-    # Dirección IP del PLC y otros parámetros de conexión
-    PLC_IP = '192.168.0.1'  # Cambia esta dirección IP por la dirección IP de tu PLC
-    RACK = 0  # Rack donde está el PLC
-    SLOT = 1  # Slot donde está el PLC
-
-    # Crear una instancia del cliente
-    plc = snap7.client.Client()
-
-    # Conectar al PLC
-    plc.connect(PLC_IP, RACK, SLOT)
-
-    # Verificar si la conexión fue exitosa
-    if plc.get_connected():
-        print("Conectado al PLC")
-
-        # Leer la información de un bloque específico
-        block_type = BlocksList['DB']  # Tipo de bloque (ejemplo: DB)
-        block_number = 1  # Número del bloque (ejemplo: DB1)
-
-        try:
-            block_info = plc.get_block_info(block_type, block_number)
-
-            # Mostrar la información obtenida
-            print("Block Type:", block_info.BlockType)
-            print("Block Number:", block_info.BlockNumber)
-            print("Length:", block_info.Length)
-            print("Load Size:", block_info.LoadSize)
-            print("MC7 Size:", block_info.MC7Size)
-            print("Load Memory Address:", block_info.LoadMemoryAddress)
-            print("Local Data:", block_info.LocalData)
-            print("SBB Length:", block_info.SBBLength)
-            print("Checksum:", block_info.Checksum)
-            print("Version:", block_info.Version)
-
-        except snap7.snap7exceptions.Snap7Exception as e:
-            print(f"Error al obtener la información del bloque: {e}")
-
-        # Desconectar del PLC
-        plc.disconnect()
-
-    else:
-        print("No se pudo conectar al PLC")
+from snap7.type import Area
 
 
 def main():
     plc = S7_Manager(ip_address='192.168.0.1')
     print(plc.get_cpu_info())
-    db2_bytearray = plc.read_area(Area.DB, 2, 0, 416)
-    db2 = Analog_Filt_Scale(db2_bytearray)
-    print(db2)
 
-    # result = plc.
-    # print(result)
-    # print(snap7.util.get_real(result,46))
-    # print(plc.read_area())
+    print("read 1: original data")
+    db2_bytearray = plc.read_area(Area.DB, 2, 0, 416)  # read db raw data from PLC
+    db2 = Analog_Filt_Scale(db2_bytearray)  # interpret raw data as Analog_Filt_Scale object
+    print(db2.read_values())  # read values from Analog_Filt_Scale object
+    db2.Cfg_High_Range = 999.0  # modify attributes
+    db2.Cfg_Low_Range = 111.0  # modify attributes
+    db2.Cfg_High_Proc = 888.0  # modify attributes
+    db2.Cfg_Low_Proc = 222.0  # modify attributes
+    db2.Cfg_High_Warn = 777.0  # modify attributes
+    db2.Cfg_Low_Warn = 333.0  # modify attributes
+    print(db2.read_values())
+    db2_bytearray = db2.write_values()  # write Analog_Filt_Scale object attribute values to raw data
+    plc.write_area(Area.DB, 2, 0, db2_bytearray)  # write db raw data to PLC
+
+    print("read 2: modified data")
+    db2_bytearray_ = plc.read_area(Area.DB, 2, 0, 416)
+    db2_ = Analog_Filt_Scale(db2_bytearray_)
+    print(db2_.read_values())
 
 
 def solisPLC_read_db():
@@ -92,6 +59,4 @@ def solisPLC_write_db():
 
 if __name__ == "__main__":
     main()
-    # solisPLC_read_db()
-    # solisPLC_write_db()
-    # solisPLC_read_db()
+
